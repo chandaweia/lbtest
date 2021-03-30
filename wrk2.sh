2ab5d925731b02674e66410f0b86d66572626653
chandaweia <chandaweia@hotmail.com>
2021-03-30 21:06:52 +0000

    v2

--- wrk2.sh
+++ wrk2.sh
@@ -11,7 +11,7 @@ URL_Heavy=http://13.0.0.29:8081/high/web730m.html
 Duration=180
 DurationLarge=20
 #let R_Light=20000
-let R_Light=2*${Flow_Light}
+let R_Light=${Flow_Light}
 RESULT_DIR=./wrk6
 SCRIPT_DIR=./scripts
 echo "R_Light:"$R_Light
